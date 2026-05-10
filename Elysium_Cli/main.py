f61cc77f27f516f09489b768cc90db10332c9fba
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-05-10 18:39:50 +0545

    migrating to python and re wrote it

---
+++ Elysium_Cli/main.py
@@ -0,0 +1,13 @@
+from internal.core.core import main
+
+# soo here is a story i want to make this cli ai based and looking at future integration C aint gonna cut it 
+# so i am chnaging to python again ! i know this is a 3rd shift in lang but hey i want a function cli ok! and 
+# it will be agentic . and also i might add functionEL tomorrow for logical execution of the command ! 
+#
+
+
+
+
+
+if __name__ == "__main__":
+    main()
