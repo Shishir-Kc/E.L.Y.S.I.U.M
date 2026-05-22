247eb24ea3dc3fb74db06317125541446e86a201
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-05-22 18:58:58 +0545

    this wont be here

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 REPO="https://github.com/Shishir-Kc/Elysium"
 INSTALL_DIR="$HOME/.Elysium"
-
+CONFIG_DIR="$HOME/.config/Elysium"
 echo "Installing Elysium..."
 
 if [ -d "$INSTALL_DIR" ]; then
@@ -13,4 +13,11 @@ fi
 
 git clone "$REPO" "$INSTALL_DIR"
 
-echo "Elysium installed to $INSTALL_DIR"
+echo "Installing E.L.Y.S.I.U.M "
+echo "Almost there . . .  . "
+if [-d "$CONFIG_DIR"]; then
+  exit 1
+fi
+echo "Creating configs "
+mkdir -p $CONFIG_DIR
+
