ff85e048841ebe942b0e826d0d1d8a35e6748a4a
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-05-29 17:45:11 +0545

    chor: illustrating code flow

--- Agents/agent.py
+++ Agents/agent.py
@@ -1,1 +1,17 @@
 
+
+class Agents:
+    def __init__(self) -> None:
+        pass
+    def deep_agent(self):
+        ...
+    def web_agent(self):
+        ...
+    def worker_agent(self):
+        ...
+    def agents_council(self):
+        ...
+    def loop(self):
+        ...
+
+
