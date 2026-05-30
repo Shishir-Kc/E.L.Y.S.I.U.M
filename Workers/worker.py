9424afab6255053bf80b851628350087c3ff4d4a
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-05-30 16:37:59 +0545

    chor:specified file paths

--- Workers/worker.py
+++ Workers/worker.py
@@ -1,12 +1,21 @@
 import threading
+from Elysium_Config.path_config import ELYSIUM_PATH
+import os 
 
+
+worker_path = f"{ELYSIUM_PATH}/Config/worker"
+worker_log = f"{ELYSIUM_PATH}/Logs/worker"
+
+paths = [worker_path,worker_log]
+for path in paths:
+    os.makedirs(path,exist_ok=True)
 
 class worker:
     def __init__(self) -> None:
         pass
     def check_worker_config(self):
         pass
-    def create_worker_config(self):
+    def add_config(self):
         pass
     def load_worker_config(self):
         pass
