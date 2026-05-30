0e05f8cc8ad4daa27ebac51a714d0e65b7db2a99
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-05-30 16:24:07 +0545

    chor: illustrating worker flow

---
+++ Workers/worker.py
@@ -0,0 +1,13 @@
+import threading
+
+
+class worker:
+    def __init__(self) -> None:
+        pass
+    def check_worker_config(self):
+        pass
+    def create_worker_config(self):
+        pass
+    def load_worker_config(self):
+        pass
+
