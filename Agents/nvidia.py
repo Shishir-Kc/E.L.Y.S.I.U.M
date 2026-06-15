7ed9eb143c38dd7739ba130553625bb346a5662c
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-06-15 18:23:26 +0545

    chor: implemented nvidias LLm (base)

--- Agents/nvidia.py
+++ Agents/nvidia.py
@@ -1,30 +1,40 @@
 from openai import OpenAI
+from Agents import Load_Agent
+
+
+agent = Load_Agent()
+
 
 
 
 
 class NvidiaAgent:
-    def __init__(self) -> None:
-        self.api_key:str = ""
-        self.model:str=""
-        self.provider:str= ""
+    def __init__(self,agent) -> None:
+        roulet = agent.model_roulet(priority_provider="nvidia")
+        self.model:str=roulet['model']
+        self.provider:str=roulet['model_provider']
+        self.api_key:str =  agent.model_key(provider=self.provider,model=self.model)
         self.baseurl:str="https://integrate.api.nvidia.com/v1"
         self.client = OpenAI(
             base_url=self.baseurl,
             api_key=self.api_key
         )
+        print(f"""
+                model => {self.model} \n 
+                provider => {self.provider}
 
+              """)
 
     def chat(self,prompt):
+        print(f"{self.provider}/{self.model}")
         response = self.client.responses.create(
-            model=f"deepseek-ai/deepseek-v4-pro",
+            model=f"{self.provider}/{self.model}",
             input=prompt,
-            reasoning={'effort':'xhigh'}
+            reasoning={'effort':'high'}
         )
         return response.output_text
 
-
-agent = NvidiaAgent()
-while True:
-    print(agent.chat(prompt=input(':> ')))
-
+if __name__ == "__main__":
+    agent = Load_Agent()
+    agent = NvidiaAgent(agent)
+    print(agent.chat(prompt=input(':>')))
