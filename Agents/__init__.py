c09be6f5de075b0fc2589db6d22d670e4ff08c22
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-06-15 12:15:42 +0545

    chor: adding modelroulet

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -1,16 +1,21 @@
 from ElysiumConfig.model_config import Elysium_Model_Config as EMC
+import random
 
 class Load_Agent():
- def __init__(self):
+  def __init__(self):
     self.ElysiumModelConfig = EMC() # EMC stants for Elysium_Model_Config : ) 
     self.model_config = self.ElysiumModelConfig.load_config()
-        
-    
+  def modelroulet(self): 
+    provider  = self.ElysiumModelConfig.available_providers()
+    random_provider = random.choice(list(provider))
+    provider = provider[random_provider]
+    print(provider)
 
 
 if __name__ == "__main__":
     agent = Load_Agent()
     try:
-        print(agent.ElysiumModelConfig.insert_api_key(api_key="are_u_stupid",provider_name="google_genai",model_name="gemini-2.5-flash"))
+        # print(agent.   agent.modelroulet()ElysiumModelConfig.insert_api_key(api_key="are_u_stupid",provider_name="google_genai",model_name="gemini-2.5-flash"))
+     agent.modelroulet()
     except Exception as e:
         print(e)
