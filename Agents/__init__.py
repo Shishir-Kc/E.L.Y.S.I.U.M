d2acbca8a2eb90e9f072affaeaabd7513c98c021
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-06-15 18:22:43 +0545

    chor: implemented modelroulet

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -1,21 +1,38 @@
 from ElysiumConfig.model_config import Elysium_Model_Config as EMC
+from Errors.errors import ProviderNotFound
+
 import random
 
 class Load_Agent():
   def __init__(self):
     self.ElysiumModelConfig = EMC() # EMC stants for Elysium_Model_Config : ) 
     self.model_config = self.ElysiumModelConfig.load_config()
-  def modelroulet(self): 
-    provider  = self.ElysiumModelConfig.available_providers()
-    random_provider = random.choice(list(provider))
-    provider = provider[random_provider]
-    print(provider)
+    self.ignoreList = ['priority','installation','is_installed','requires','auth']
 
+  def model_roulet(self,priority_provider:str=""):
+    providers  = self.ElysiumModelConfig.available_providers()
+    provider = ""
+    for _,available_provider in enumerate(providers.values(),start=1):
+            if priority_provider == available_provider:
+               provider = priority_provider
+               break
+     
+    if not  priority_provider:     
+     random_provider = random.choice(list(providers))
+     provider = providers[random_provider] 
+    if not provider:
+            raise ProviderNotFound("ProviderDoesnotExistOrNotFound!") 
+    models = self.ElysiumModelConfig.load_config()
+    provider_models = models.get(provider,{})
+    [provider_models.pop(items,None) for items in self.ignoreList] # removing unwanted stuff !
+    model = random.choice(list(provider_models))
+    models = models.get(provider,{})
+    model_metadata = models.get(model,{}) 
+    return {   
+        "model_provider":provider,
+        "model":model_metadata['model_name']
+        }
 
-if __name__ == "__main__":
-    agent = Load_Agent()
-    try:
-        # print(agent.   agent.modelroulet()ElysiumModelConfig.insert_api_key(api_key="are_u_stupid",provider_name="google_genai",model_name="gemini-2.5-flash"))
-     agent.modelroulet()
-    except Exception as e:
-        print(e)
+  def model_key(self,provider:str,model:str):
+    return self.ElysiumModelConfig.load_model(required_provider=provider,required_model=model)['api_key']
+
