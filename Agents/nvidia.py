c623092afb392b8a1050873b32bd190f7dbbfc05
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-17 12:54:33 +0545

    chor:Added service provider

--- Agents/nvidia.py
+++ Agents/nvidia.py
@@ -12,8 +12,9 @@ class NvidiaAgent:
     def __init__(self,agent) -> None:
         roulet = agent.model_roulet(priority_provider="nvidia")
         self.model:str=roulet['model']
-        self.provider:str=roulet['model_provider']
-        self.api_key:str =  agent.model_key(provider=self.provider,model=self.model)
+        self.model_provider:str=roulet['model_provider']
+        self.provider:str=roulet['provider']
+        self.api_key:str =  agent.model_key(provider=self.model_provider,model=self.model)
         self.baseurl:str="https://integrate.api.nvidia.com/v1"
         self.client = OpenAI(
             base_url=self.baseurl,
@@ -26,15 +27,21 @@ class NvidiaAgent:
               """)
 
     def chat(self,prompt):
-        print(f"{self.provider}/{self.model}")
-        response = self.client.responses.create(
-            model=f"{self.provider}/{self.model}",
-            input=prompt,
-            reasoning={'effort':'high'}
-        )
-        return response.output_text
+        provider = self.model_provider
+        if self.provider:
+            provider = self.provider
+
+        model=f"{provider}/{self.model}"
+        print(model)
+        response = self.client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        stream=False
+    )
+        return response.choices[0].message.content
 
 if __name__ == "__main__":
     agent = Load_Agent()
     agent = NvidiaAgent(agent)
-    print(agent.chat(prompt=input(':>')))
+    while True:
+        print(agent.chat(prompt=input(':>')))
