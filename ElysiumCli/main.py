5669d1629f3c6a36d32bff738e831f2c2a845a77
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-07-20 15:08:29 +0545

    addeed some decsc for placeholder args

--- ElysiumCli/main.py
+++ ElysiumCli/main.py
@@ -10,31 +10,31 @@ from ElysiumCli.commands.elysium_info import (
 )
 
 def build_parser(): 
-    parser = argparse.ArgumentParser(prog="E.L.Y.S.I.U.M")
+    parser = argparse.ArgumentParser(prog="romeo")
     subparser = parser.add_subparsers(dest="command")
 
-    version_parser = subparser.add_parser("version")
+    version_parser = subparser.add_parser("version",help="gets the current version of E.L.Y.S.I.U.M")
     version_parser.set_defaults(func=version)
    
-    status_parser = subparser.add_parser("status")
+    status_parser = subparser.add_parser("status",help="gets the current Status of E.L.Y.S.I.U.M")
     status_parser.set_defaults(func=status)
 
-    dev_parser =  subparser.add_parser("dev")
+    dev_parser =  subparser.add_parser("dev",help="gets the last development update ")
     dev_parser.set_defaults(func=last_development_changes)
     
-    version_name_parser = subparser.add_parser("version_name")
+    version_name_parser = subparser.add_parser("version_name",help="gets the current version name of E.L.Y.S.I.U.M")
     version_name_parser.set_defaults(func=version_name)
 
-    stable_parser = subparser.add_parser("is_stable")
+    stable_parser = subparser.add_parser("is_stable",help="checks if current E.L.Y.S.I.U.M is stable or not")
     stable_parser.set_defaults(func=is_stable)
 
-    el_parser = subparser.add_parser("info")
+    el_parser = subparser.add_parser("info",help="gets overall info of E.L.Y.S.I.U.M")
     el_parser.set_defaults(func=elysium_info)
 
-    version_checker_parser = subparser.add_parser("check_version")
+    version_checker_parser = subparser.add_parser("check_version",help="Checks if new version is available or not ")
     version_checker_parser.set_defaults(func=check_version)
 
-    update_parser = subparser.add_parser("update")
+    update_parser = subparser.add_parser("update",help="Updates E.L.Y.S.I.U.M with the latest relaease")
     update_parser.set_defaults(func=update)
     return parser
 
