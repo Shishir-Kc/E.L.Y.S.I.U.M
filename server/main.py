90df10ca397ac6515d33fdfa34161c4242106697
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-08-20 17:58:02 +0545

    renamed

---
+++ server/main.py
@@ -0,0 +1,60 @@
+from fastapi import (FastAPI,status,
+                    WebSocket,WebSocketDisconnect)
+from fastapi.responses import StreamingResponse
+from contextlib import asynccontextmanager
+import asyncio
+from pathlib import Path
+import logging 
+import os 
+ 
+testdir = Path.home() / ".config"/"E.L.Y.S.I.U.M"/"Logs"/"Server"/ "server.log"
+
+
+logger = logging.getLogger("uvicorn.error")
+logger.setLevel(logging.DEBUG)
+formater = logging.Formatter("| %(asctime)s | %(levelname)s | %(message)s |")
+os.makedirs(testdir.parent,exist_ok=True)
+filehandlar = logging.FileHandler(testdir)
+filehandlar.setFormatter(formater)
+logger.addHandler(filehandlar)
+
+
+@asynccontextmanager
+async def lifespan(server:FastAPI):
+    logger.info("Booting E.L.Y.S.I.U.M FastAPI Server ")
+    yield
+
+server = FastAPI(title="E.L.Y.S.I.U.M",lifespan=lifespan)
+
+@server.get("/")
+async def base():
+    return {
+    "status":status.HTTP_200_OK
+}
+
+async def logstream():
+    with open(testdir,"r") as f:
+        f.seek(0,1)
+        while True:
+            line = f.readline()
+            if not line:
+                await asyncio.sleep(0.5)
+                continue
+
+            yield f"data:{line}\n\n"
+
+
+@server.get("/read")
+async def readLog():
+    return StreamingResponse(logstream(),media_type="text/event-stream") 
+
+@server.websocket("/ws")
+async def test_socket(websocket:WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Echo = {data}")
+
+    except WebSocketDisconnect:
+        print("disconnected")
